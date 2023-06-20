625b88641a0eeac9ff6e2658453d03ea55a0bec4
mubezi <nesthermubezi2@gmail.com>
2023-06-20 12:05:39 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet-print the alphabet
+ *
+ * return 0
+ *
+ */
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar(alp);
+
+	_putchar('\n');
+
+}
