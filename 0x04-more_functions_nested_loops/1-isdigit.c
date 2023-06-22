21dbbc656cef649524c2e06c236b304f370c8631
mubezi <nesthermubezi2@gmail.com>
2023-06-22 09:56:44 -0700

    prints number through 0 to 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit btn 0 and 1
+ * @c: integer to print
+ *
+ * Return: On success 1.
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
