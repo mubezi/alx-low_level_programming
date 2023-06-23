004aa96e2609b69909dd881cd2459599aa7022a5
mubezi <nesthermubezi2@gmail.com>
2023-06-23 06:42:32 -0700

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main-calculate the largest prime number
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	long int i;
+	long int num;
+	long int max;
+
+	num = 612852475143;
+	max = -1;
+
+	while (num % 2 == 0)
+	{
+		max = 2;
+		num /= 2;
+	}
+
+	for (i = 3; i * i <= num; i = i + 2)
+	{
+		while (num % i == 0)
+		{
+			max = i;
+			num = num / 2;
+		}
+	}
+	if (num > 2)
+		max = num;
+
+	printf("%ld\n", max);
+
+	return (0);
+}
