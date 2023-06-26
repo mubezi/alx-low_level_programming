69e2eab14651e3a7524150103b2181faf0c4d5ba
mubezi <nesthermubezi2@gmail.com>
2023-06-26 15:27:47 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,21 +9,20 @@
  */
 void puts_half(char *str)
 {
-	int len = strlen(str);
-	int start;
+	int a, n, longi;
 
-	if (len % 2 == 0)
-	{
-		start = len / 2;
-	}
-	else if (len % 2 == 1)
-	{
-		start = (len - 1) / 2;
-	}
-	while (str[start] != '\0')
-	{
-		putchar(str[start]);
-		start++;
-	}
-	putchar('\n');
+	longi = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
+
+	n = (longi / 2);
+
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
 }
+
