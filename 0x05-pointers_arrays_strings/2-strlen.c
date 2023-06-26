7c411ba49119e09ad48cbe9a2a6ece0571cad6c7
mubezi <nesthermubezi2@gmail.com>
2023-06-26 11:57:26 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - find the length of a character
+ * @s: pointer for char
+ *
+ * Return: lenth of char
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+		len++;
+
+	return (len);
+}
