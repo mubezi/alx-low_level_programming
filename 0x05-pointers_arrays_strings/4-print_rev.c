a2c227e638e7f15af27517ea1306c0e74e197d4d
mubezi <nesthermubezi2@gmail.com>
2023-06-26 13:03:45 -0700

    print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+ * print_rev - function to print reverse
+ * @s: string to be reverse
+ *
+ * Return -void
+ */
 void print_rev(char *s)
 {
 	int len = 0;
+
 	while (*s != '\0')
 	{
 		len++;
